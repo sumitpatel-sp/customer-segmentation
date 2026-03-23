641ed93b958a4e4b6ed4d4c09b72fbb0df43a66f
Sumit Patel <sumitpatel060704@gmail.com>
2026-03-23 21:20:52 +0530

    trained on new dataset

--- src/evaluate.py
+++
@@ -1,6 +0,0 @@
-from sklearn.metrics import silhouette_score
-
-def evaluate_model(model, data):
-    labels = model.predict(data)
-    score = silhouette_score(data, labels)
-    print("Silhouette Score:", score)
